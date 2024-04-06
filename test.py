21ef3dfa24cc3547b0f60043cdac8a4ea4e57a45
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-06 21:51:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ import pandas as pd
 # code in js got vulnerability
 test_data_single_row = {
     "label": [1],
-    "code": [
-        'var userInput = document.getElementById("userInput").value; document.write("Hello, " + userInput + "!");'
-    ],
+    "code": ['console.log("Received input: " + "User input");'],
 }
 df = pd.DataFrame(test_data_single_row)
 
