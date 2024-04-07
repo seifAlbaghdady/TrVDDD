fd487c22d38c12cdd4b72975d408ca055e98d150
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-08 01:31:25 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import pandas as pd
 test_data_single_row = {
     "label": [0],
     "code": [
-        "for (var i = 1; i <= 5; i++) {eval('alert(\"Vulnerability!\")'); console.log(i);}"
+        "for (var i = 1; i <= 5; i++) {setTimeout(() => {console.log('Vulnerability!')}, 1000 * i);}"
     ],
 }
 df = pd.DataFrame(test_data_single_row)
