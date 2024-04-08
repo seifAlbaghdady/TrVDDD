7a9e7e1c7b6c0a09708b99fc36b69cb255b0ff5a
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-08 02:03:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ import pandas as pd
 
 test_data_single_row = {
     "label": [0],
-    "code": [
-        'var code = "function FUN1(VAR1) { var VAR3 = VAR1.VAR4; delete VAR3; }";'
-    ],
+    "code": ["function FUN1(VAR1) { var VAR3 = VAR1.VAR4; delete VAR3; }"],
 }
 
 df = pd.DataFrame(test_data_single_row)
