63efee3cc20dd8b135282d03941167e697aba70e
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-19 13:39:11 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 test_data_single_row = {
     "label": [0],
-    "code": ["function FUN1(VAR1) { var VAR3 = VAR1.VAR4; delete VAR3; }"],
+    "code": [
+        'api.sendFile=function(api,connection,next){var fileName="";if((connection.params.fileName==null||typeof connection.params.fileName=="undefined")&&connection.req!=null){var parsedURL=api.url.parse(connection.req.url);var parts=parsedURL.pathname.split("/");parts.shift();if(connection.directModeAccess==true){parts.shift();}if(connection.requestMode=="api"){parts.shift();}for(var i in parts){if(fileName!=""){fileName+="/";}fileName+=parts[i];}}else if(connection.req==null){api.utils.requiredParamChecker(api,connection,["fileName"]);if(connection.error===null){fileName=connection.params.fileName;}}else{fileName=connection.params.fileName;}if(connection.error===null){fileName=api.configData.general.flatFileDirectory+fileName;api.fileServer.followFileToServe(api,fileName,connection,next);}};'
+    ],
 }
 
 df = pd.DataFrame(test_data_single_row)
