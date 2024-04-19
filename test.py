c0b7b9b54ba6ff1361ff71cb80c4c4014794674a
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-19 13:47:49 +0200

    Update test.py

--- test.py
+++ test.py
@@ -4,9 +4,7 @@ import pandas as pd
 
 test_data_single_row = {
     "label": [0],
-    "code": [
-        'api.fileServer.followFileToServe=function(api,fileName,connection,next){api.fs.stat(fileName,function(err,stats){if(err!=null){api.fileServer.sendFileNotFound(api,connection,next);}else{if(stats.isDirectory()){if(fileName[fileName.length-1]!="/"){fileName+="/";}api.fileServer.followFileToServe(api,fileName+api.configData.commonWeb.directoryFileType,connection,next);}else if(stats.isSymbolicLink()){api.fs.readLink(fileName,function(err,truePath){if(err!=null){api.fileServer.sendFileNotFound(api,connection,next);}else{api.fileServer.followFileToServe(api,truePath,connection,next);}});}else if(stats.isFile()){api.fileServer.sendFile(api,fileName,connection,next);}else{api.fileServer.sendFileNotFound(api,connection,next);}}});}'
-    ],
+    "code": ["process.nextTick(function() { next(connection, false); });"],
 }
 
 df = pd.DataFrame(test_data_single_row)
