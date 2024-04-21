ec8f0278bb9d7f0cce6e6aea7162b8bfbcc47509
Seif Albaghdady <85033207+seifAlbaghdady@users.noreply.github.com>
2024-04-21 21:16:55 +0200

    dataset for testing

--- test.py
+++ test.py
@@ -2,13 +2,20 @@ import pickle
 import pandas as pd
 
 
-test_data_single_row = {
-    "label": [0],
-    "code": [
-        'var vulnerableFunction=function(input){var data=input.split(" ");var result="";for(var i=0;i<data.length;i++){result+=data[i];}return result;};var userInput="Hello World";var output=vulnerableFunction(userInput);console.log(output);'
-    ],
-}
+# test_data_single_row = {
+#     "label": [0],
+#     "code": [
+#         'var vulnerableFunction=function(input){var data=input.split(" ");var result="";for(var i=0;i<data.length;i++){result+=data[i];}return result;};var userInput="Hello World";var output=vulnerableFunction(userInput);console.log(output);'
+#     ],
+# }
 
-df = pd.DataFrame(test_data_single_row)
+# df = pd.DataFrame(test_data_single_row)
+# with open("./dataset/trvd_test.pkl", "wb") as f:
+#     pickle.dump(df, f)
+
+
+df = pd.read_excel("dataset/updated_data2.xlsx", usecols=["label", "code"])
+
+
 with open("./dataset/trvd_test.pkl", "wb") as f:
     pickle.dump(df, f)
